b38fe676e1cf630ece1526c8f66e9a1df8e429e0
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-25 11:20:06 -0300

    feat: Projeto criado - Conexão ao GitHub

---
+++ src/main/java/com/devlucasvargas/sistema_lanchonete/SistemaLanchoneteApplication.java
@@ -0,0 +1,13 @@
+package com.devlucasvargas.sistema_lanchonete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLanchoneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLanchoneteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlucasvargas/sistema_lanchonete/SistemaLanchoneteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucasvargas.sistema_lanchonete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLanchoneteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
