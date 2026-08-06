15ef337192a5cdb1fffa654130074672f3bc9f6a
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-08-06 09:23:00 -0300

    feature: add nova classe/tabela ItemPedido

---
+++ src/main/java/com/devlucasvargas/sistema_lanchonete/models/ItemPedido.java
@@ -0,0 +1,32 @@
+package com.devlucasvargas.sistema_lanchonete.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_item_pedido")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemPedido {
+
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private PedidoModel pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private ProdutoModel produto;
+
+    private Integer quantidade;
+    private Double precoUnitario;
+    private Double subTotal;
+
+}
